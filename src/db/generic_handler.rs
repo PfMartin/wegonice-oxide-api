907c79a44f2b7b7e9f88e39fbba9bbf6cd584d86
PfMartin <martinpfatrisch@gmail.com>
2024-11-23 17:06:30 +0100

    Added get_multiple generic method

--- src/db/generic_handler.rs
+++ src/db/generic_handler.rs
@@ -1,16 +1,36 @@
 use super::mongo_db_handler::MongoDbHandler;
 use anyhow::{anyhow, Result};
 use bson::{doc, oid::ObjectId};
+use futures_util::TryStreamExt;
 use serde::de::DeserializeOwned;
 use std::{convert::Into, marker::Sync};
 
 pub trait GenericHandler {
+    async fn get_multiple<T, S>(&self, collection_name: &str) -> Result<Vec<S>>
+    where
+        T: Sync + Send + DeserializeOwned + Into<S> + Copy;
     async fn get_by_id<T, S>(&self, id: &str, collection_name: &str) -> Result<S>
     where
         T: Sync + Send + DeserializeOwned + Into<S>;
 }
 
 impl GenericHandler for MongoDbHandler {
+    async fn get_multiple<T, S>(&self, collection_name: &str) -> Result<Vec<S>>
+    where
+        T: Sync + Send + DeserializeOwned + Into<S> + Copy,
+    {
+        let cursor = self
+            .db
+            .collection::<T>(collection_name)
+            .find(doc! {})
+            .await?;
+
+        let db_documents = cursor.try_collect::<Vec<T>>().await?;
+        let documents = db_documents.iter().map(|&d| d.into()).collect::<Vec<S>>();
+
+        Ok(documents)
+    }
+
     async fn get_by_id<T, S>(&self, id: &str, collection_name: &str) -> Result<S>
     where
         T: Sync + Send + DeserializeOwned + Into<S>,
