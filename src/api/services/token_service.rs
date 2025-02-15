019fcc3c2588ba1c144aa27cc26ba44f2ee8e137
PfMartin <martinpfatrisch@gmail.com>
2025-02-15 12:30:59 +0100

    Added decode function for jwt token with unit test

--- src/api/services/token_service.rs
+++ src/api/services/token_service.rs
@@ -1,15 +1,17 @@
 use anyhow::Result;
 use chrono::{Duration, Utc};
-use jsonwebtoken::{encode, EncodingKey, Header};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
 
 use crate::model::user::{Role, UserAuthInfo};
 
+// TODO: Create OOP service
+
 #[derive(Serialize, Deserialize)]
-struct Claims {
-    sub: String,
-    role: Role,
-    exp: i64,
+pub struct Claims {
+    pub sub: String,
+    pub role: Role,
+    pub exp: i64,
 }
 
 pub fn generate_jwt(
@@ -34,10 +36,23 @@ pub fn generate_jwt(
     Ok(token)
 }
 
+pub fn decode_jwt(token: &str, token_key: &str) -> Result<Claims> {
+    let decoded_token = decode::<Claims>(
+        &token,
+        &DecodingKey::from_secret(token_key.as_ref()),
+        &Validation::default(),
+    )?;
+
+    let decoded_claims = decoded_token.claims;
+
+    Ok(decoded_claims)
+}
+
 #[cfg(test)]
 mod unit_tests_token_service {
-    use jsonwebtoken::{decode, DecodingKey, Validation};
     use pretty_assertions::assert_eq;
+
+    use crate::test_utils::assert_date_in_range;
 
     use super::*;
 
@@ -62,6 +77,46 @@ mod unit_tests_token_service {
 
         assert_eq!(decoded_claims.sub, user_auth_info.email.to_string());
         assert_eq!(decoded_claims.role, user_auth_info.role);
+
+        Ok(())
+    }
+
+    #[test]
+    fn decodes_jwt() -> Result<()> {
+        let email = "test@email.com";
+        let role = Role::User;
+        let token_validity_duration_h = 1;
+
+        let claims = Claims {
+            sub: email.into(),
+            role,
+            exp: (Utc::now() + Duration::hours(token_validity_duration_h)).timestamp(),
+        };
+
+        let secret = "testSecret";
+
+        let token = encode(
+            &Header::default(),
+            &claims,
+            &EncodingKey::from_secret(secret.as_bytes()),
+        )?;
+
+        let claims = decode_jwt(&token, secret)?;
+
+        assert_eq!(
+            claims.sub,
+            String::from(email),
+            "Incorrect email from claims"
+        );
+        assert_eq!(claims.role, role, "Incorrect role from claims");
+        assert!(claims.exp > (Utc::now() + Duration::hours(1)).timestamp() - 1000);
+
+        assert_date_in_range(
+            claims.exp,
+            (Utc::now() + Duration::hours(1)).timestamp(),
+            1000,
+            "Claims in time range",
+        )?;
 
         Ok(())
     }
