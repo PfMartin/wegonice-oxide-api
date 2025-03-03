6a82aa2901a5c945a605217dee9a5a2d82955788
PfMartin <martinpfatrisch@gmail.com>
2025-03-03 14:33:30 +0100

    Additionally tests heart beat response body

--- tests/integration_tests_heartbeat.rs
+++ tests/integration_tests_heartbeat.rs
@@ -1,4 +1,7 @@
+mod common;
+
 use anyhow::Result;
+use common::ResponseBody;
 use pretty_assertions::assert_eq;
 use reqwest::StatusCode;
 
@@ -8,6 +11,11 @@ async fn heartbeat_request_successful() -> Result<()> {
 
     assert_eq!(res.status().is_success(), true);
     assert_eq!(res.status(), StatusCode::OK);
+
+    let response_body = res.json::<ResponseBody>().await?;
+
+    assert_eq!(response_body.data, Some(String::from("Ok")));
+    assert_eq!(response_body.error, String::from(""));
 
     Ok(())
 }
