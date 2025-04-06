c4144f43a62118aabd7ca30d2d606232a858346f
PfMartin <martinpfatrisch@gmail.com>
2025-04-06 12:34:04 +0200

    Added more testcases to login

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use bson::{doc, oid::ObjectId, DateTime};
 use dotenv;
-use mongodb::{options::ClientOptions, Client};
+use mongodb::{options::ClientOptions, Client, Collection};
 use serde::{Deserialize, Serialize};
 use std::env;
 
@@ -64,7 +64,7 @@ pub fn get_config(config_path: Option<&str>) -> Result<Config> {
     })
 }
 
-pub async fn register_user() -> Result<String> {
+pub async fn get_users_collection() -> Result<Collection<UserMongoDb>> {
     let config = get_config(Some(".env"))?;
 
     let uri = format!(
@@ -86,18 +86,34 @@ pub async fn register_user() -> Result<String> {
     let db = client.database(&config.db_name);
     let users_collection = db.collection::<UserMongoDb>("users");
 
+    Ok(users_collection)
+}
+
+pub async fn register_user(is_activated: bool) -> Result<String> {
+    let collection = get_users_collection().await?;
+
     // Password: test_password
     let user = UserMongoDb {
         _id: ObjectId::new(),
         email: "loginUser@email.com".into(),
         password_hash: "$argon2id$v=19$m=19456,t=2,p=1$8/I4UD0qo5joX/Vd5R9TfA$ryIDC5fe3axKWvC4qLsznYsEUjgqu+xvgV3aX/mCJOI".into(),
         role: Role::User,
-        is_activated: true,
+        is_activated,
         created_at: DateTime::now(),
         modified_at: DateTime::now()
     };
 
-    let insert_result = users_collection.insert_one(&user).await?;
+    let insert_result = collection.insert_one(&user).await?;
 
     Ok(insert_result.inserted_id.to_string())
+}
+
+pub async fn delete_users() -> Result<u64> {
+    let collection = get_users_collection().await?;
+
+    let filter = doc! {};
+
+    let delete_result = collection.delete_many(filter).await?;
+
+    Ok(delete_result.deleted_count)
 }
--- tests/integration_tests_login.rs
+++ tests/integration_tests_login.rs
@@ -1,40 +1,87 @@
 mod common;
 
 use anyhow::Result;
-use common::{get_config, register_user, AuthPayload, ResponseBody};
+use common::{delete_users, get_config, register_user, AuthPayload, ResponseBody};
 use pretty_assertions::assert_eq;
 use reqwest::{Client, StatusCode};
 
 #[tokio::test]
-async fn login_success() -> Result<()> {
-    let _ = register_user().await?;
-
-    let login_payload = AuthPayload {
-        email: "loginUser@email.com".into(),
-        password: "test_password".into(),
-    };
-
+async fn login() -> Result<()> {
     let config = get_config(Some(".env"))?;
 
-    let client = Client::new();
-    let res = client
-        .post(format!("http://{}/auth/login", config.server_host))
-        .json(&login_payload)
-        .send()
-        .await?;
+    struct TestCase {
+        title: String,
+        login_payload: AuthPayload,
+        response_code: StatusCode,
+        is_user_active: bool,
+        is_success: bool,
+    }
 
-    assert_eq!(res.status().is_success(), true);
-    assert_eq!(res.status(), StatusCode::ACCEPTED);
+    let test_cases = vec![
+        TestCase {
+            title: "Successfully logs user in".into(),
+            login_payload: AuthPayload {
+                email: "loginUser@email.com".into(),
+                password: "test_password".into(),
+            },
+            is_user_active: true,
+            is_success: true,
+            response_code: StatusCode::ACCEPTED,
+        },
+        TestCase {
+            title: "Fails to login with wrong password".into(),
+            login_payload: AuthPayload {
+                email: "loginUser@email.com".into(),
+                password: "wrong_password".into(),
+            },
+            is_user_active: true,
+            is_success: false,
+            response_code: StatusCode::UNAUTHORIZED,
+        },
+        TestCase {
+            title: "Fails to login with wrong password".into(),
+            login_payload: AuthPayload {
+                email: "wrongUser@email.com".into(),
+                password: "test_password".into(),
+            },
+            is_user_active: true,
+            is_success: false,
+            response_code: StatusCode::NOT_FOUND,
+        },
+        TestCase {
+            title: "Fails to login with wrong password".into(),
+            login_payload: AuthPayload {
+                email: "loginUser@email.com".into(),
+                password: "test_password".into(),
+            },
+            is_user_active: false,
+            is_success: false,
+            response_code: StatusCode::UNAUTHORIZED,
+        },
+    ];
 
-    let response_body = res.json::<ResponseBody>().await?;
+    for t in test_cases {
+        let _ = register_user(t.is_user_active).await?;
+        let client = Client::new();
+        let res = client
+            .post(format!("http://{}/auth/login", config.server_host))
+            .json(&t.login_payload)
+            .send()
+            .await?;
 
-    assert_eq!(response_body.data.is_some(), true);
-    assert_eq!(response_body.error, String::from(""));
+        assert_eq!(res.status().is_success(), t.is_success, "{}", &t.title);
+        assert_eq!(res.status(), t.response_code, "{}", &t.title);
+
+        let response_body = res.json::<ResponseBody>().await?;
+
+        assert_eq!(response_body.data.is_some(), t.is_success, "{}", &t.title);
+
+        if t.is_success {
+            assert_eq!(response_body.error, String::from(""), "{}", &t.title);
+        }
+
+        delete_users().await?;
+    }
 
     Ok(())
-}
-
-#[test]
-fn login_fail() {
-    assert_eq!(2 == 1, false);
 }
